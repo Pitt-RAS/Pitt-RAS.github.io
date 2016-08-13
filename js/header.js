29eb5a8972dfe456d1418541e30b94ccde53dc92
John Linahan <linahan.john@gmail.com>
2016-08-13 12:25:27 -0400

    Header

---
+++ js/header.js
@@ -0,0 +1,7 @@
+$("#header__hamburger").click(function() {
+	$("#header__nav").addClass("header__nav--active");
+})
+
+$("#header__exit").click(function() {
+	$("#header__nav").removeClass("header__nav--active");
+})
---
+++ js/main.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+
+	$.getScript("/js/header.js");
+});
