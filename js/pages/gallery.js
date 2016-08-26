3fecade9bd97a38dae721110cd3ae614372c1a0d
John Linahan <linahan.john@gmail.com>
2016-08-26 11:43:14 -0400

    Desktop styles

--- js/pages/gallery.js
+++ js/pages/gallery.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function() {
     {% for album in site.data.gallery %}
-        prepareFlickrAlbum("{{ album.albumID }}", "{{ album.htmlID }}", 999);
+        prepareFlickrAlbum("{{ album.albumID }}", "{{ album.htmlID }}", 999, undefined, 1);
     {% endfor %}
 });
 
