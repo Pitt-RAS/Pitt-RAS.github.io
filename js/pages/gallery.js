bdbd3987c378671abaddf6a0a3d0052679c618a9
Aaron Miller <acmiller273@gmail.com>
2017-10-04 01:10:39 -0400

    Switched to facebook photos

--- js/pages/gallery.js
+++ js/pages/gallery.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function() {
     {% for album in site.data.gallery %}
-        prepareFlickrAlbum("{{ album.albumID }}", "{{ album.htmlID }}", 999, undefined, 1);
+        prepareFacebookAlbum("{{ album.albumID }}", "{{ album.htmlID }}", 999, undefined, 1);
     {% endfor %}
 });
 
